5dafa0fea92cf62bcec1ea1882489f0dfdafdbb4
Marco Santini <marcosantinia12@gmail.com>
2024-02-06 21:31:27 -0700

    Added functionality to hamburger menu.Created overlay

--- script.js
+++ script.js
@@ -1,15 +1,39 @@
+const overlay = document.querySelector('.overlay')
+const openMenu2 = document.querySelector('.openMenu2')
+const closeMenu2 = document.querySelector('.closeMenu2')
 const select = document.querySelector('.select');
 const caret = document.querySelector('.caret');
 const menu = document.querySelector('.menu');
+const menu2 = document.querySelector('.menu2');
 const options = document.querySelectorAll('.menu li');
+const options2 = document.querySelectorAll('.menu2 li');
 const selected = document.querySelector('.selected');
 
 select.addEventListener('click', function () {
     console.log('select clicked');
-
     menu.classList.toggle('menu-open');
     caret.classList.toggle('caret-rotate');
 });
+
+openMenu2.addEventListener('click', function () {
+    console.log('open clicked');
+    openMenu2.style.display = 'none';
+    closeMenu2.style.display = 'block';
+    overlay.style.display = 'block';
+    menu.classList.add('menu2');
+    menu.classList.add('menu-open');
+});
+
+closeMenu2.addEventListener('click', function () {
+    console.log('open clicked');
+    closeMenu2.style.display = 'none';
+    openMenu2.style.display = 'block';
+    overlay.style.display = 'none';
+    menu.classList.remove('menu2');
+    menu.classList.remove('menu-open');
+
+});
+
 
 options.forEach(option => {
     option.addEventListener('click', function () {
@@ -20,9 +44,11 @@ options.forEach(option => {
         options.forEach(option => {
             option.classList.remove('active');
         });
+
         option.classList.add('active');
     });
-
 });
+
+
 
 
